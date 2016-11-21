d6c34395d9910e92965ab610ad7c7b0e105eef50
Darin London <darin.london@duke.edu>
2016-11-21 08:56:22 -0500

    official versions

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -10,7 +10,7 @@ public class InsultGenerator {
 		String vowels = "AEIOU";
 		String article = "an";
 		String theInsult = "";
-
+		
 		try {
 			String databaseURL = "jdbc:postgresql://";
 			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
@@ -21,18 +21,18 @@ public class InsultGenerator {
 			Connection connection = DriverManager.getConnection(databaseURL, username, password);
 
 			if (connection != null) {
-				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a, long_adjective b, noun c ORDER BY random() limit 1";
+				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(SQL);
 				while (rs.next()) {
-					if ( vowels.indexOf( rs.getString("first").charAt(0) ) == -1 ) {
+					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
 						article = "a";
 					}
-					theInsult = String.format("Thou art %s %s %s %s!", article,
-					rs.getString("first"), rs.getString("second"), rs.getString("noun"));
-					rs.close();
-					connection.close();
+					theInsult =  String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
+							rs.getString("second"), rs.getString("noun"));
 				}
+				rs.close();
+				connection.close();
 			}
 		} catch (Exception e) {
 			return "Database connection problem!";
